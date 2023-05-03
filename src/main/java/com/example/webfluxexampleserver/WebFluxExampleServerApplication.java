aef8732b248497a848b3a277a790fc4e8f887d0a
borris starodubtsev <boris.starodubtsev@adorsis.com>
2023-05-03 14:52:42 +0200

    Initial commit

---
+++ src/main/java/com/example/webfluxexampleserver/WebFluxExampleServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.webfluxexampleserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFluxExampleServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebFluxExampleServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/webfluxexampleserver/WebFluxExampleServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webfluxexampleserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFluxExampleServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
